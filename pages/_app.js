04020c3df1863f4679f15f495bc9cbbc56d566c8
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2020-05-03 21:56:25 +0400

    delete getinitialprops

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,6 @@ import React from 'react'
 import { PageTransition } from 'next-page-transitions'
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
- 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
- 
-    return { pageProps }
-  }
 
   render() {
     const { Component, pageProps,router } = this.props

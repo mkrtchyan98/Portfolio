f7f15c96cc81b361525dcf376e820dba004812b6
mkrtchyan98 <tigran.mkrtchyan2398@gmail.com>
2020-07-03 00:11:33 +0400

    added new project

--- pages/projects.js
+++ pages/projects.js
@@ -11,51 +11,74 @@ const About = () => {
 	<article id="projects">
 <div className="responsive">
   <div className="gallery">
-    	<img src="/images/reactquiz.jpg" alt="Quiz" />
-    <div className="desc">
-  <h3>quiz app </h3>
-       <h6>React,Redux,OpenTB API</h6>
+      <img src="/images/infospacex.jpg" alt="shopping" />
+        <div className="desc">
+     <h3>SpaceX Info webpage</h3>
+     <h6> React ,Apollo, React Spring, material-ui, bootstrap</h6>
+<a className="btn btn-primary" href="https://infospacex.vercel.app/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
+  <a className="btn btn-primary" href="https://github.com/mkrtchyan98/SpaceInfo" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
 
-	<a className="btn btn-primary" href="https://react-quiz.mkrtchyan98.now.sh/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
-		<a className="btn btn-primary" href="https://github.com/mkrtchyan98/React-quiz" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
+
 
     </div>
   </div>
-</div>
+</div>  
+
 <div className="responsive">
   <div className="gallery">
-    	<img src="/images/shopping.png" alt="shopping" />
+   <img src="/images/react_memory.jpg" alt="memory app" />
+    <div className="desc">
+     <h3>React Memory game</h3>
+     <h6> react hooks</h6>
+<a className="btn btn-primary" href="https://react-memory-game-iota.now.sh/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
+  <a className="btn btn-primary" href="https://github.com/mkrtchyan98/React-memory-game" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
+    </div>
+  </div>
+</div>  
+
+    <div className="responsive">
+  <div className="gallery">
+      <img src="/images/netflixclone.jpg" alt="Netflix" />
+    <div className="desc">
+  <h3>Netflix website clone </h3>
+       <h6>React,material-ui,styled-components,MovieDB API</h6>
+
+  <a className="btn btn-primary" href="https://netflix-clone.now.sh/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
+    <a className="btn btn-primary" href="https://github.com/mkrtchyan98/netflixclone" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
+    </div>
+  </div>
+</div>
+
+<div className="responsive">
+  <div className="gallery">
+    	<img src="/images/reactquiz.jpg" alt="Quiz" />
+    <div className="desc">
+  <h3>Quiz website </h3>
+       <h6>React, Redux, OpenTB API</h6>
+	<a className="btn btn-primary" href="https://react-quiz.mkrtchyan98.now.sh/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
+		<a className="btn btn-primary" href="https://github.com/mkrtchyan98/React-quiz" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
+    </div>
+  </div>
+</div>
+
+<div className="responsive">
+  <div className="gallery">
+    	<img src="/images/shopping.jpg" alt="shopping" />
         <div className="desc">
-     <h3>react-shop</h3>
+     <h3>React Shop</h3>
      <h6> React,Redux,material-ui,bootstrap</h6>
 <a className="btn btn-primary" href="https://react-shop.onrender.com" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
 	<a className="btn btn-primary" href="https://github.com/mkrtchyan98/React-Shopping-Cart" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
-
-
-
     </div>
   </div>
 </div>								
 
 
-								<div className="responsive">
-  <div className="gallery">
-    	<img src="/images/netflixclone.jpg" alt="Netflix" />
-    <div className="desc">
-  <h3>netflix website clone </h3>
-       <h6>React,material-ui,styled-components,MovieDB API</h6>
-
-	<a className="btn btn-primary" href="https://netflix-clone.now.sh/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
-		<a className="btn btn-primary" href="https://github.com/mkrtchyan98/netflixclone" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
-
-    </div>
-  </div>
-</div>
 <div className="responsive">
   <div className="gallery">
-    	<img src="/images/chatonee.png" alt="Chatonee" />
+    	<img src="/images/chatonee.jpg" alt="Chatonee" />
         <div className="desc">
-     <h3>chatonee(chat app)</h3>
+     <h3>Chatonee(chat app)</h3>
      <h6> React,Firebase,material-ui</h6>
 <a className="btn btn-primary" href="https://chatonnez.firebaseapp.com/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
 	<a className="btn btn-primary" href="https://github.com/mkrtchyan98/Chat-app" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
@@ -65,23 +88,11 @@ const About = () => {
     </div>
   </div>
 </div>	
+ 
+			
 <div className="responsive">
   <div className="gallery">
-   <img src="/images/react-memory.png" alt="memory app" />
-    <div className="desc">
-     <h3>React Memory game</h3>
-     <h6> react hooks</h6>
-<a className="btn btn-primary" href="https://react-memory-game-iota.now.sh/" role="button" target="_blank"  rel="noopener noreferrer" >See Website</a>
-  <a className="btn btn-primary" href="https://github.com/mkrtchyan98/React-memory-game" role="button" target="_blank"  rel="noopener noreferrer" >See Code</a>
-
-
-    </div>
-  </div>
-</div>   
-							
-<div className="responsive">
-  <div className="gallery">
-   <img src="/images/Math_quest.png" alt="Mat-quest app" />
+   <img src="/images/Math_quest.jpg" alt="Mat-quest app" />
     <div className="desc">
      <h3>Mat-quest</h3>
      <h6> react hooks,material-ui </h6>
